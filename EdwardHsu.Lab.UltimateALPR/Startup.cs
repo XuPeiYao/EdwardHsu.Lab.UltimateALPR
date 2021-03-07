245b18b5040833b82a6f7b9c1bbac4be4717888c
Edward Hsu <st89382000@gmail.com>
2021-03-07 19:18:22 +0800

    feat: Swagger on production and fix same bug

--- EdwardHsu.Lab.UltimateALPR/Startup.cs
+++ EdwardHsu.Lab.UltimateALPR/Startup.cs
@@ -41,9 +41,10 @@ namespace EdwardHsu.Lab.UltimateALPR
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EdwardHsu.Lab.UltimateALPR v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EdwardHsu.Lab.UltimateALPR v1"));
 
             app.UseHttpsRedirection();
 
